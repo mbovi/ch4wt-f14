44862347e45984bcb9a89709631d7a97bc4b381c
Richard Ilson <rilson@uncc.edu>
2014-10-01 15:15:54 -0400

    start of new ch 4 walk-thru

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Ch4wtF14::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ch4wtF14::Application.config.secret_token = '71e043bdf6e79f319477862374143fc52fc819add63b7782d87e478b8f845d6328dc8023be86c068fbea693bf93b751170a798f759f5e3307dc02cf6a2d52e7e'
